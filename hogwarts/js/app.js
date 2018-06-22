756c53a7e1b52574760dfd5c42205533f56f5fbe
mac <mac@macs-MacBook-Pro.local>
2018-06-21 19:05:41 -0600

    Commit 4 - I realize I can make things that are ever increasingly more complex with greater ease. Adding order is always more effort than causing chaos

--- hogwarts/js/app.js
+++ hogwarts/js/app.js
@@ -9,12 +9,12 @@ $( document ).ready(function() {
     //console.log( "ready!" );
     // let $container = $("#container");
     // let $h1 = $("h1");
-    $("body").append("<h2>Cameron Binion</h2>");
+    $("body").prepend("<h2>Cameron Binion</h2>");
     $("body").append("<h3>Slytherin</h3>");
     $("body").append("<h4>Hawthorn Wand with Dragon Heartstring Core</h4>");
     $("body").append("<h4 id=Owl>Flappy</h4>");
     $("body").append("<ul><li>Butterbeer</li><li id =secret>Invisibility Cloak</li><li id =secret>Magic Map</li><li id=secret>Time Turner</li><li id=Owl>Leash</li><li>Bertie Bott's Every Flavor (Jelly) Beans</li>");
-    
+
 });
 
 
